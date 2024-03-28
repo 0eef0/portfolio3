ad9623b5f651290c9e246af4d46fd7f7f2522b4a
TheCapn-MEC <erolda266@west-mec.org>
2024-03-27 23:47:16 -0700

    finished player movement, need to do snake movement next

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,25 @@ import Portfolio from "./Components/Portfolio";
 import Projects from "./Components/Projects";
 
 const App = () => {
+  // Variable denoting the player's position
+  const [ playerPos, setPlayerPos ] = useState(1);
+  const changePlayerPos = (direction) => {
+    if(direction === 'down') {
+      setPlayerPos((playerPos === 2) ? playerPos : playerPos + 1);
+    } else {
+      setPlayerPos((playerPos === 0) ? playerPos : playerPos - 1);
+    }
+  };
+
   // Variable denoting which mode the site is currently on
   // 0 will be Baby Defender, 1 will be portfolio, 2 will be projects
   const [ mode, setMode ] = useState(0);
 
+  // Returns the component of the correct mode
   const getContent = () => {
     switch(mode) {
       case 0:
-        return <Game/>;
+        return <Game playerPos={ playerPos } mode={ mode } />;
       case 1:
         return <Portfolio/>
       case 2:
@@ -24,6 +35,32 @@ const App = () => {
         break;
     }
   };
+
+  useEffect(() => {
+    let fired = false;
+    document.addEventListener("keydown", (e) => {
+      console.log(e.key);
+      if(!fired) {
+        fired = true;
+        switch(e.key) {
+          case 'f':
+            changePlayerPos('up');
+            break;
+          case 'j':
+            changePlayerPos('down');
+            break;
+          default:
+            break;
+        }
+      }
+    });
+    document.addEventListener("keyup", (e) => {
+      console.log(e.key);
+      if(fired) {
+        fired = false;
+      }
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -36,7 +73,7 @@ const App = () => {
       </div>
       <div className="leftBtn">
         <p className="btnLabel"><FontAwesomeIcon icon={ faCircleUp } /> Up</p>
-        <button className="btn"></button>
+        <button className="btn" onClick={() => { setPlayerPos(1); changePlayerPos('up')}}></button>
       </div>
       <div className="screen">
         <p className="gameName">BABY DEFENDER</p>
@@ -61,7 +98,7 @@ const App = () => {
       </div>
       <div className="rightBtn">
         <p className="btnLabel">Down <FontAwesomeIcon icon={ faCircleDown } /></p>
-        <button className="btn"></button>  
+        <button className="btn" onClick={() => { setPlayerPos(1); changePlayerPos('down')}}></button>  
       </div>
     </div>
   );
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const Game = () => {
+const Game = ({ playerPos, mode }) => {
     /*
         This 2D array represents the game state
         1 means blank tile
@@ -11,18 +11,40 @@ const Game = () => {
         -3 means snake_full
     */
     const [ gameState, setGameState ] = useState([
-        [2,2,2,2,2],
-        [2,2,2,2,2],
-        [2,2,2,2,2]
+        [3,1,1,1,-1],
+        [3,2,1,1,-1],
+        [3,1,1,1,-1]
     ]);
+    const [ prevPos, setPrevPos ] = useState(playerPos);
+    const [ snakesPos, setSnakesPos] = useState([4,4,4]);
+    useEffect(() => {
+        let temp = gameState;
+        temp[prevPos][1]--;
+        temp[playerPos][1]++;
+        setPrevPos(playerPos);
+        setGameState(temp);
+    }, [playerPos]);
+
+    // reset the player position when the mode is changed
+    useEffect(() => {
+        setPrevPos(1);
+    }, [mode]);
 
   return (
     <div className='gameState'>
+        <div className="scoreboard">
+            <h1>TOP: 0000</h1>
+            <h1>SCORE: 0000</h1>
+        </div>
         {
-            gameState.map(row => {
-                return row.map(col => {
-                    return <img src={require(`../Assets/sprite(${col}).png`)} />
-                })
+            gameState.map((row, i) => {
+                return <div key={i}> 
+                    {
+                        row.map((col, j) => {
+                            return (col !== 1) ? <img src={require(`../Assets/sprite(${col}).png`)} alt={col} key={`r${i}c${j}`} /> : <img src={require(`../Assets/sprite(2).png`)} alt={col} key={`r${i}c${j}`} className="spacer" />;
+                        })
+                    }
+                </div>
             })
         }
     </div>
